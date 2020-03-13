2eb434c9b762b96ead2d41605317ef8b3d0d1e1e
rory-roccio <rory.roccio@gmail.com>
2020-03-13 15:09:01 -0700

    Fix esLint formatting issues

--- server/db/config.js
+++ server/db/config.js
@@ -2,12 +2,12 @@ const Promise = require('bluebird');
 
 module.exports = (db) => {
   Promise.promisifyAll(db);
-  return db.queryAsync(`DROP TABLE IF EXISTS reservations, restaurants;`)
+  return db.queryAsync('DROP TABLE IF EXISTS reservations, restaurants;')
     .then(() => {
       db.queryAsync(`CREATE TABLE restaurants (
         id INTEGER NOT NULL AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(40),
-        max_seats INTEGER);`)
+        max_seats INTEGER);`);
     })
     .then(() => {
       db.queryAsync(`CREATE TABLE reservations (
@@ -15,22 +15,12 @@ module.exports = (db) => {
         id_restaurants INTEGER,
         party_size INTEGER,
         date_time DATETIME,
-        FOREIGN KEY (id_restaurants) REFERENCES restaurants (id));`)
+        FOREIGN KEY (id_restaurants) REFERENCES restaurants (id));`);
     })
     .then(() => {
-      console.log(`Successfully configured database`)
+      console.log('Successfully configured database');
     })
     .catch((err) => {
       console.log(err.stack);
-    })
-}
-
-
-		
-
-
-
-		
-
-
-
+    });
+};
