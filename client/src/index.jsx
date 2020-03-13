8bf63c9ec79153a5ca65a1f15751551f092a01e4
rory-roccio <rory.roccio@gmail.com>
2020-03-13 16:58:16 -0700

    Add index.jsx and render App

---
+++ client/src/index.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable import/extensions */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './components/App.jsx';
+
+// eslint-disable-next-line no-undef
+ReactDOM.render(<App />, document.getElementById('app'));
