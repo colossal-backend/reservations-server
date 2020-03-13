62ffc63ce81c308adbb8a02c43b88f50c2189f49
rory-roccio <rory.roccio@gmail.com>
2020-03-13 16:58:38 -0700

    Add webpack config with babel and jsx rules

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+const SRC_DIR = path.join(__dirname, 'client', 'src');
+const PUBLIC_DIR = path.join(__dirname, 'public');
+
+module.exports = {
+  mode: 'development',
+  entry: path.join(SRC_DIR, 'index.jsx'),
+  output: {
+    filename: 'main.js',
+    path: PUBLIC_DIR,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
+  },
+};
