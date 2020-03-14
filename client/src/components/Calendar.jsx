fd13ea1e67a29fb35c26413a3b57f86589ffce23
rory-roccio <rory.roccio@gmail.com>
2020-03-14 10:39:46 -0700

    Add Calendar component

---
+++ client/src/components/Calendar.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+const Calendar = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [available, setAvailable] = useState(true);
+  return (
+    <div>
+      <h2>Calendar</h2>
+    </div>
+  );
+};
+
+export default Calendar;
