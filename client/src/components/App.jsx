e716523583dbbd407a1d569e9c4da8211f607f24
rory-roccio <rory.roccio@gmail.com>
2020-03-16 10:37:29 -0700

    Add child components

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-
-import Calendar from './Calendar';
+import Title from './Title';
+import DateSelector from './DateSelector';
+import TimeSelector from './TimeSelector';
+import PartySelector from './PartySelector';
+import ReserveButton from './ReserveButton';
 
 const App = () => (
   <div>
-    <h1>
-      Hello from App.jsx
-    </h1>
-    <Calendar />
+    <h1> App </h1>
+    <Title />
+    <DateSelector />
+    <TimeSelector />
+    <PartySelector />
+    <ReserveButton />
   </div>
 );
 
