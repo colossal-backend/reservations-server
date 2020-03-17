aafc2de1d97419b862fe719ca11c6876a2bc4f5d
rory-roccio <rory.roccio@gmail.com>
2020-03-17 13:29:06 -0700

    Add styles

--- client/src/components/Title.jsx
+++ client/src/components/Title.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const TitleWrapper = styled.div`
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  font-size: 12px;
+  margin-left: 10px;
+  color: #333333;
+`;
 
 const Title = () => (
-  <div>
-    <h1> Title </h1>
-  </div>
+  <TitleWrapper>
+    <h2 data-test="title">Make a Reservation</h2>
+  </TitleWrapper>
 );
 
 export default Title;
