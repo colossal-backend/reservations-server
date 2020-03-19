7683e59ee45af76b1d41db55c38843d20c83db77
rory-roccio <rory.roccio@gmail.com>
2020-03-18 18:37:46 -0700

    Add styles, calendar rendering on click (later must factor out state)

--- client/src/components/DateSelector.jsx
+++ client/src/components/DateSelector.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 import Calendar from './Calendar';
 
-const DateSelector = () => (
-  <div>
-    <h1> DateSelector </h1>
-    {/* <Calendar /> */}
-  </div>
-);
+// Tue, Mar 17
+
+const SelectWrapper = styled.div`
+  width: 235px;
+  height: 18px;
+  display: block;
+  padding: 5px 24px 5px 32px;
+  margin-bottom: 3px;
+  padding-top: 7px;
+  padding-bottom: 3px;
+  border-radius: 3px;
+  border: 1px solid #999999;
+  border-radius: 3px;
+  background-color: white;
+  font-size: 13.33px;
+  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+`;
+
+const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+const newDate = new Date();
+const today = {
+  month: newDate.getMonth(),
+  day: newDate.getDay(),
+};
+
+const DateSelector = () => {
+  const [date, setDate] = useState(today);
+  const [calendar, setCalendar] = useState(false);
+
+  return (
+    <SelectWrapper onClick={() => { setCalendar(!calendar); }}>
+      { `${days[date.day]}, ${months[date.month]} ${date.day.toString()}` }
+      { calendar ? <Calendar setDate={setDate} /> : null }
+    </SelectWrapper>
+  );
+};
+// https://fecreservations.s3-us-west-1.amazonaws.com/calendar.png
 
 export default DateSelector;
