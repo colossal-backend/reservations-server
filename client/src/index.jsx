9c7d4077fae94cd2563db87d264d2f2d74683ca3
rory-roccio <rory.roccio@gmail.com>
2020-03-19 14:11:52 -0700

    Remove prop assignments

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,4 +4,4 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 
 // eslint-disable-next-line no-undef
-ReactDOM.render(<App newDate={newDate} allMonths={allMonths} allDaysOfWeek={allDaysOfWeek} />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById('app'));
