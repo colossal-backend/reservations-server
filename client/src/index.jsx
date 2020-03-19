55d3f7a9f073b650f0071156386da3616a37c305
rory-roccio <rory.roccio@gmail.com>
2020-03-19 14:09:43 -0700

    Refactor App props into App (default props)

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,9 +3,5 @@ import ReactDOM from 'react-dom';
 
 import App from './components/App';
 
-const newDate = new Date();
-const allMonths = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-const allDaysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-
 // eslint-disable-next-line no-undef
 ReactDOM.render(<App newDate={newDate} allMonths={allMonths} allDaysOfWeek={allDaysOfWeek} />, document.getElementById('app'));
