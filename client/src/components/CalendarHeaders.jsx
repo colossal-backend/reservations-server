b1d251b64b9a9746686d718257d2348836622938
rory-roccio <rory.roccio@gmail.com>
2020-03-20 12:57:30 -0700

    Alter font weight and border

--- client/src/components/CalendarHeaders.jsx
+++ client/src/components/CalendarHeaders.jsx
@@ -3,11 +3,13 @@ import styled from 'styled-components';
 
 const RowWrapper = styled.div`
   display: table-row;
+  border-bottom: 1px solid #e6e6e6;
 `;
 
 const DayHeader = styled.div`
   display: table-cell;
   width: 13.5%;
+  font-weight: 700;
   font-size: 13px;
   text-align: center;
   color: #d32323;
