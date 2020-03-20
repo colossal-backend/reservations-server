fcedb83f058773e044b364be9a157510ef5323c1
rory-roccio <rory.roccio@gmail.com>
2020-03-20 15:49:32 -0700

    Remove unnecessary state, create setDays function, pass to dateSelector

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -24,15 +24,9 @@ class App extends React.Component {
     super({ newDate, allMonths, allDaysOfWeek });
     this.state = {
       timeOptions: [],
-      prevMonthDays: [],
-      currentMonthDays: [],
-      nextMonthDays: [],
+      days: [],
       months: moment.months(),
-      currentDayOfWeek: newDate.format('D'),
-      currentMonth: newDate.format('MMMM'),
-      currentYear: newDate.format('Y'),
-      selectedDate: null,
-      selectedTime: newDate.format('h:mm a'),
+      selectedDate: newDate,
       selectedPartySize: 2,
     };
 
@@ -40,13 +34,15 @@ class App extends React.Component {
     this.setTimeOptions = this.setTimeOptions.bind(this);
     this.setSelectedTime = this.setSelectedTime.bind(this);
     this.setSelectedPartySize = this.setSelectedPartySize.bind(this);
+    this.setDays = this.setDays.bind(this);
   }
 
   componentDidMount(time = this.props.newDate.format('h:mm a')) {
     this.setTimeOptions(time);
+    this.setDays();
   }
 
-  setTimeOptions(time = this.selectedTime) {
+  setTimeOptions(time = this.state.selectedDate.format('h:mm a')) {
     const newTimeOptions = this.getTimeOptions(time);
     this.setState((state) => ({ ...state, timeOptions: newTimeOptions }));
   }
@@ -82,11 +78,59 @@ class App extends React.Component {
     return timeOptions;
   }
 
+  setDays() {
+    const prevDays = []; // will capture all overflow days from prev month
+    const currDays = []; // will capture all days from current month
+    const nextDays = []; // will capture all overflow days from next month
+    const weeks = []; // will hold all days grouped into weeks
+    const prevMonth = moment(this.props.newDate).format('M') - 1;
+    const nextMonth = moment(this.props.newDate).format('M') + 1;
+    const firstDayIndex = moment(this.props.newDate).startOf('month').format('d');
+    const lastDayIndex = moment(this.props.newDate).endOf('month').format('d');
+    const daysInMonth = this.props.newDate.daysInMonth();
+    const currentDay = this.state.selectedDate.day();
+
+    const Day = (prev, curr, next, date, available, selected) => ({
+      prev, curr, next, date, available, selected,
+    });
+    // Add previous Days
+    for (let i = 0; i < firstDayIndex; i += 1) {
+      prevDays.push(Day(true, false, false, 'xx', null, false));
+    }
+    // Add current Days
+    for (let i = 1; i < daysInMonth + 1; i += 1) {
+      if (i < currentDay) {
+        prevDays.push(Day(false, true, false, i, null, false));
+      } else if (i === currentDay) {
+        prevDays.push(Day(false, true, false, i, null, true));
+      } else {
+        prevDays.push(Day(false, true, false, i, null, false));
+      }
+    }
+    // Add next Days
+    for (let i = 6 - lastDayIndex; i > 0; i -= 1) {
+      nextDays.push(Day(false, false, true, 'yy', null, false));
+    }
+    // Put all days together to group into weeks
+    const allDays = [...prevDays, ...currDays, ...nextDays];
+    // Add weeks
+    let tempWeek = [];
+    allDays.forEach((day) => {
+      tempWeek.push(day);
+      if (tempWeek.length === 7) {
+        weeks.push(tempWeek);
+        tempWeek = [];
+      }
+    });
+    // setState
+    this.setState((state) => ({ ...state, days: [...weeks] }));
+  }
+
   render() {
     return (
       <AppWrapper>
         <Title />
-        <DateSelector />
+        <DateSelector matrixOfDays={this.state.days} />
         <TimeSelector timeOptions={this.state.timeOptions} setSelectedTime={this.setSelectedTime} />
         <PartySelector setSelectedPartySize={this.setSelectedPartySize} />
         <ReserveButton />
