b732e0fb94f5f348bc8265cd8414c58c74bf1c8e
rory-roccio <rory.roccio@gmail.com>
2020-03-22 17:23:37 -0700

    Pass next month moment object to Calendar

--- client/src/components/DateSelector.jsx
+++ client/src/components/DateSelector.jsx
@@ -20,13 +20,13 @@ const SelectWrapper = styled.div`
   font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
 `;
 
-const DateSelector = ({ selectedDate, setSelectedDate, thisMonth, nextMonth, today}) => {
+const DateSelector = ({ selectedDate, setSelectedDate, thisMonth, nextMonth, today, nextMoment}) => {
   const [calendar, setCalendar] = useState(false);
 
   return (
     <SelectWrapper onClick={() => { setCalendar(!calendar); }}>
       { `${selectedDate.format('ddd')}, ${selectedDate.format('MMM')} ${selectedDate.format('D')}` }
-      { calendar ? <Calendar selectedDate={selectedDate} setSelectedDate={setSelectedDate} thisMonth={thisMonth} nextMonth={nextMonth} currentDate={today} /> : null }
+      { calendar ? <Calendar selectedDate={selectedDate} setSelectedDate={setSelectedDate} thisMonth={thisMonth} nextMonth={nextMonth} currentDate={today} nextMoment={nextMoment}/> : null }
     </SelectWrapper>
   );
 };
