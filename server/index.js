26b72ca65ac8bb39602a52a21e01f98dcce28205
rory-roccio <rory.roccio@gmail.com>
2020-03-24 15:46:29 -0700

    Change port

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const model = require('./db/reservations.model.js');
 
 const app = express();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3004;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
