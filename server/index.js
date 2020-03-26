19947fcbf0e926b9d8107db12e8ad7471ab8e9b1
rory-roccio <rory.roccio@gmail.com>
2020-03-26 11:22:33 -0700

    Set port to 5000

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const model = require('./db/reservations.model.js');
 
 const app = express();
 
-const PORT = process.env.PORT || 3004;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../public')));
