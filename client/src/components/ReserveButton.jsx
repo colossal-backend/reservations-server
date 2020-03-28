265d2a51f6a8329f6905efd90bc95bc58c55c69b
rory-roccio <rory.roccio@gmail.com>
2020-03-28 15:37:09 -0700

    Change relative position

--- client/src/components/ReserveButton.jsx
+++ client/src/components/ReserveButton.jsx
@@ -4,14 +4,16 @@ import PropTypes from 'prop-types';
 
 const ButtonWrapper = styled.button`
   background-color: #d32323;
+  position: relative;
+  bottom: 28px;
   width: 290px;
   height: 30px;
-  display: block;
   padding: 5px 24px 5px 30px;
   margin-top: 3px;
   border-radius: 3px;
   border: 1px solid #d32323;
   border-radius: 3px;
+  outline: 0;
   cursor: pointer;
   color: white;
   font-weight: 700;
