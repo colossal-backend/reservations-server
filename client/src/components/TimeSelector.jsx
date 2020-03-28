7fadcbbc65883a84f7053ddc7829bfa298cc19f0
rory-roccio <rory.roccio@gmail.com>
2020-03-28 15:37:18 -0700

    Add Icon

--- client/src/components/TimeSelector.jsx
+++ client/src/components/TimeSelector.jsx
@@ -3,27 +3,39 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const TimeSelectorWrapper = styled.div`
-  display: inline-block;
+  position: relative;
+  border: 1px solid #cccccc;
+  border-radius: 3px;
+  width: 142px;
+  height: 27px;
 `;
 
 const SelectWrapper = styled.select`
-  width: 144px;
-  height: 30px;
-  padding: 5px 24px 5px 30px;
-  border-radius: 3px;
-  border-color: #cccccc;
-  background-color: white;
-  font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+  position: relative;
+  bottom: 18px;
+  width: 100%;
+  height: 100%;
+  padding: 0px 0px 0px 25px;
+  overflow: visible !important;
+  background-color: transparent;
+  border: none;
+  outline: 0;
 `;
 
-// const ImgWrapper = styled.svg`
-//   position: absolute;
-//   top: 3px;
-//   left: 3px;
-// `;
+const Icon = styled.img`
+  display: inline-block;
+  position: relative;
+  left: 9px;
+  top: 3px;
+  vertical-align: middle;
+  height: 13px;
+  width: 13px;
+  z-index: 3;
+`;
 
 const TimeSelector = ({ timeOptions, setSelectedTime }) => (
   <TimeSelectorWrapper>
+    <Icon src="https://fecreservations.s3-us-west-1.amazonaws.com/clock.png" />
     <SelectWrapper onChange={(e) => { e.preventDefault(); setSelectedTime(e.target.value); }}>
       {timeOptions.map((time) => (<option key={time}>{time}</option>))}
     </SelectWrapper>
