512ecd6f816e0151796811556b2347987476bbdc
TJ Casner <casnertj@gmail.com>
2020-04-12 22:14:14 -0700

    updates to reservation server

--- server/index.js
+++ server/index.js
@@ -4,86 +4,61 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-require('dotenv').config();
-// const cluster = require('cluster');
-// const numCPUs = require('os').cpus().length;
+// require('dotenv').config();
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 const Controller = require('./controllers');
 
 const app = express();
-const port = process.env.PORT || 5050;
+const port = 5050;
 
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, '../public')));
 
-// if (cluster.isMaster) {
-//   console.log(`Master ${process.pid} is running`);
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running`);
 
-//   // Fork workers from CPUs
-//   for (let i = 0; i < numCPUs; i += 1) {
-//     cluster.fork();
-//   }
-
-//   // Check worker status
-//   cluster.on('exit', (worker, code, signal) => {
-//     console.log(`worker ${worker.process.pid} died`);
-//   });
-// } else {
-//   // Workers can share any TCP connection
-//   // Express initialiization
-//   console.log(`Worker ${process.pid} started`);
-
-//   // Get Available Reservations
-//   app.get('/reservations/:restaurantId/:partySize', Controller.getAvailable);
-
-//   // Get all restaurant reservations
-//   app.get('/reservations/:restaurantId', Controller.getReservations);
-
-//   // Save Reservation
-//   app.post('/reservations', Controller.post);
-
-//   // // Update Reservation
-//   app.patch('/reservations/:id/update', Controller.update);
-
-//   // // Delete Reservation
-//   app.delete('/reservations/:id/delete', Controller.destroy);
-
-
-//   app.listen(port, (err) => {
-//     if (err) {
-//       console.log(`Failed to start server: ${err}`);
-//     } else {
-//       console.log(`Listening on ${port}`);
-//     }
-//   });
-// }
-
-
-app.listen(port, (err) => {
-  if (err) {
-    console.log(`Failed to start server: ${err}`);
-  } else {
-    console.log(`Listening on ${port}`);
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i += 1) {
+    cluster.fork();
   }
-});
 
-// Loader.io
-app.get('/loaderio-e64828b65028448238cb43dfadf96936', (req, res) => {
-  res.sendFile(`${__dirname}/loaderio-e64828b65028448238cb43dfadf96936.txt`);
-});
+  // Check if work id is died
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died`);
+  });
+} else {
+  // This is Workers can share any TCP connection
+  // It will be initialized using express
+  console.log(`Worker ${process.pid} started`);
 
-// Get Available Reservations
-app.get('/reservations/:restaurantId/:partySize', Controller.getAvailable);
+  // Loader.io
+  app.get('/loaderio-e64828b65028448238cb43dfadf96936', (req, res) => {
+    res.sendFile(`${__dirname}/loaderio-e64828b65028448238cb43dfadf96936.txt`);
+  });
 
-// Get all restaurant reservations
-app.get('/reservations/:restaurantId', Controller.getReservations);
+  // Get Available Reservations
+  app.get('/reservations/:restaurantId/:partySize', Controller.getAvailable);
 
-// Save Reservation
-app.post('/reservations', Controller.post);
+  // Get all restaurant reservations
+  app.get('/reservations/:restaurantId', Controller.getReservations);
 
-// // Update Reservation
-app.patch('/reservations/:id/update', Controller.update);
+  // Save Reservation
+  app.post('/reservations', Controller.post);
 
-// // Delete Reservation
-app.delete('/reservations/:id/delete', Controller.destroy);
+  // // Update Reservation
+  app.patch('/reservations/:id/update', Controller.update);
+
+  // // Delete Reservation
+  app.delete('/reservations/:id/delete', Controller.destroy);
+
+  app.listen(port, (err) => {
+    if (err) {
+      console.log(`Failed to start server: ${err}`);
+    } else {
+      console.log(`Listening on ${port}`);
+    }
+  });
+}
